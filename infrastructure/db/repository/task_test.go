dd73d0e9e9386e18dfbd8138b281b6d106037464
takumi616 <ohohyeah321@icloud.com>
2025-08-20 00:19:13 +0900

    Add unit test for Task updating and deleting query

--- infrastructure/db/repository/task_test.go
+++ infrastructure/db/repository/task_test.go
@@ -244,3 +244,165 @@ func TestSelectById(t *testing.T) {
 		})
 	}
 }
+
+func TestUpdate(t *testing.T) {
+	type expected struct {
+		task *domain.Task
+		err  error
+	}
+
+	testTable := map[string]struct {
+		id        string
+		input     *domain.Task
+		mockSetup func(sqlmock.Sqlmock, string, *domain.Task)
+		expected  expected
+	}{
+		"ok": {
+			id: "6a30b9b0-18bf-47b4-bd23-d72726864def",
+			input: &domain.Task{
+				Description: "Update Test Description",
+				Status:      true,
+			},
+			mockSetup: func(m sqlmock.Sqlmock, id string, input *domain.Task) {
+				rows := sqlmock.NewRows([]string{"id", "title", "description", "status"}).
+					AddRow("6a30b9b0-18bf-47b4-bd23-d72726864def", "Test Title", "Update Test Description", true)
+
+				m.ExpectQuery(regexp.QuoteMeta(
+					"UPDATE tasks SET description=$1, status=$2 WHERE id=$3 RETURNING *",
+				)).
+					WithArgs(input.Description, input.Status, id).
+					WillReturnRows(rows)
+			},
+			expected: expected{
+				task: &domain.Task{
+					Id:          "6a30b9b0-18bf-47b4-bd23-d72726864def",
+					Title:       "Test Title",
+					Description: "Update Test Description",
+					Status:      true,
+				},
+				err: nil,
+			},
+		},
+		"invalidId": {
+			id: "abc123",
+			input: &domain.Task{
+				Description: "Update Test Description",
+				Status:      true,
+			},
+			mockSetup: func(m sqlmock.Sqlmock, id string, input *domain.Task) {
+				sqlmock.NewRows([]string{"id", "title", "description", "status"}).
+					AddRow("6a30b9b0-18bf-47b4-bd23-d72726864def", "Test Title", "Test Description", false)
+
+				m.ExpectQuery(regexp.QuoteMeta(
+					"UPDATE tasks SET description=$1, status=$2 WHERE id=$3 RETURNING *",
+				)).
+					WithArgs(input.Description, input.Status, id).
+					WillReturnError(errors.New("pq: invalid input syntax for type uuid: \"abc123\""))
+			},
+			expected: expected{
+				task: nil,
+				err:  errors.New("failed to copy columns: pq: invalid input syntax for type uuid: \"abc123\""),
+			},
+		},
+	}
+
+	for n, tt := range testTable {
+		tt := tt
+		t.Run(n, func(t *testing.T) {
+			db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherRegexp))
+			require.NoError(t, err)
+			defer db.Close()
+
+			tt.mockSetup(mock, tt.id, tt.input)
+
+			repo := &TaskRepository{Db: db}
+			result, err := repo.Update(context.Background(), tt.id, tt.input)
+
+			if n == "ok" {
+				assert.Equal(t, tt.expected.task, result)
+				assert.Nil(t, err)
+			} else if n == "invalidId" {
+				assert.Nil(t, result)
+				assert.Error(t, err)
+				assert.EqualError(t, err, tt.expected.err.Error())
+			}
+
+			assert.NoError(t, mock.ExpectationsWereMet())
+		})
+	}
+}
+
+func TestDelete(t *testing.T) {
+	type expected struct {
+		task *domain.Task
+		err  error
+	}
+
+	testTable := map[string]struct {
+		id        string
+		mockSetup func(sqlmock.Sqlmock, string)
+		expected  expected
+	}{
+		"ok": {
+			id: "6a30b9b0-18bf-47b4-bd23-d72726864def",
+			mockSetup: func(m sqlmock.Sqlmock, id string) {
+				rows := sqlmock.NewRows([]string{"id"}).
+					AddRow("6a30b9b0-18bf-47b4-bd23-d72726864def")
+
+				m.ExpectQuery(regexp.QuoteMeta(
+					"DELETE FROM tasks WHERE id=$1 RETURNING id",
+				)).
+					WithArgs(id).
+					WillReturnRows(rows)
+			},
+			expected: expected{
+				task: &domain.Task{
+					Id: "6a30b9b0-18bf-47b4-bd23-d72726864def",
+				},
+				err: nil,
+			},
+		},
+		"invalidId": {
+			id: "abc123",
+			mockSetup: func(m sqlmock.Sqlmock, id string) {
+				sqlmock.NewRows([]string{"id"}).
+					AddRow("6a30b9b0-18bf-47b4-bd23-d72726864def")
+
+				m.ExpectQuery(regexp.QuoteMeta(
+					"DELETE FROM tasks WHERE id=$1 RETURNING id",
+				)).
+					WithArgs(id).
+					WillReturnError(errors.New("pq: invalid input syntax for type uuid: \"abc123\""))
+			},
+			expected: expected{
+				task: nil,
+				err:  errors.New("failed to copy columns: pq: invalid input syntax for type uuid: \"abc123\""),
+			},
+		},
+	}
+
+	for n, tt := range testTable {
+		tt := tt
+		t.Run(n, func(t *testing.T) {
+			db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherRegexp))
+			require.NoError(t, err)
+			defer db.Close()
+
+			tt.mockSetup(mock, tt.id)
+
+			repo := &TaskRepository{Db: db}
+			result, err := repo.Delete(context.Background(), tt.id)
+
+			if n == "ok" {
+				assert.Equal(t, tt.expected.task, result)
+				assert.Nil(t, err)
+			} else if n == "invalidId" {
+				assert.Nil(t, result)
+				assert.Error(t, err)
+				assert.EqualError(t, err, tt.expected.err.Error())
+			}
+
+			assert.NoError(t, mock.ExpectationsWereMet())
+		})
+	}
+}
